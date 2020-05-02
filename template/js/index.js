fafb9032a3150720b8bad446a3020a0e753fc66d
Ruben Nogueira <40404708+rubnogueira@users.noreply.github.com>
2020-05-02 20:39:34 +0100

    Fix report

--- template/js/index.js
+++ template/js/index.js
@@ -10,16 +10,15 @@ function initializeReports(){
 
     let addLetter = item.artifacts !== 1 ? "s" : ""
 
-    text += `<a href="../../ModuleOutput/AndroidForensics/${item.link}" class="text-muted list-group-item list-group-item-action">`;
-    text += `<div class="py-1 mb-0 small">`;
-    text += `<div class="d-flex justify-content-between align-items-center w-100">`;
-    text += `<strong class="text-gray-dark">Report for ${app_name} (${item.app_id})</strong>`;
-    text += `<span>${item.artifacts} artifact${addLetter}</span>`
-    text += `</div>`;
-    text += `<span class="d-block">Generated at ${date}</span>`;
-    
-    text += `</div>`;
-    text += `</a>`;
+    text += `<a href="../../ModuleOutput/AndroidForensics/${item.link}" class="text-muted list-group-item list-group-item-action">
+      <div class="py-1 mb-0 small">
+        <div class="d-flex justify-content-between align-items-center w-100">
+          <strong class="text-gray-dark">Report for ${app_name} (${item.app_id})</strong>
+          <span>${item.artifacts} artifact${addLetter}</span>
+        </div>
+        <span class="d-block">Generated at ${date}</span>
+      </div>
+    </a>`;
   });
 
   $("#reports-list").html(text);
