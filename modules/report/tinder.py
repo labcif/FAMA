594b97cf389649c5633517c6a3b65d68eae8a3b3
98jfran <55167756+98jfran@users.noreply.github.com>
2020-05-03 03:44:52 +0100

    added sqlparser, photos and bio changes to tinder module

--- modules/report/tinder.py
+++ modules/report/tinder.py
@@ -15,7 +15,9 @@ class ModuleReport(ModuleParent):
         
     def generate_report(self):
         self.report["freespace"] = self.get_undark_db()
-        self.report["profile"] = self.get_user_profile()
+        self.report["sqlparse"] = self.get_sqlparse()
+        self.report["user_photos"] = self.get_user_photos()
+        self.report["bio_changes"] = self.get_bio_changes()
         self.report["messages"] = self.get_user_messages()
         self.report["credit_cards"] = self.get_credit_cards()
         self.report["locations"] = self.get_locations()
@@ -25,16 +27,15 @@ class ModuleReport(ModuleParent):
         Utils.save_report(os.path.join(self.report_path, "Report.json"), self.report)
         return self.report
 
-    #TIKTOK
+    
     def get_user_messages(self):
         logging.info("Getting User Messages...")
 
         db = os.path.join(self.internal_cache_path, "databases", "tinder-3.db")
 
         database = Database(db)
-        messages_list =[] #each entry means 1 conversation, including participants information and messages
-            #messages from conversations
-        messages = database.execute_query("select message_to_id, message_from_id , message_text, message_sent_date, case when message_is_liked = 0 then 'Not liked' when message_is_liked = 1 then 'Liked' else message_is_liked end, case when message_is_seen = 0 then 'Not seen' when message_is_seen = 1 then 'Seen' else message_is_seen end, message_delivery_status from message_view order by message_sent_date;")
+        messages_list =[] 
+        messages = database.execute_query("select message_to_id, message_from_id , message_text, message_sent_date/1000 as message_sent_date, case when message_is_liked = 0 then 'Not liked' when message_is_liked = 1 then 'Liked' else message_is_liked end, case when message_is_seen = 0 then 'Not seen' when message_is_seen = 1 then 'Seen' else message_is_seen end, message_delivery_status from message_view order by message_sent_date;")
         
         #getting messages from conversations
         for entry in messages:
@@ -52,30 +53,46 @@ class ModuleReport(ModuleParent):
 
         return messages_list
 
-    def get_user_profile(self):
+
+
+    
+    def get_user_photos(self):
+        
+        logging.info("Get User Photos...")
+        db = os.path.join(self.internal_cache_path, "databases", "tinder-3.db").encode('utf-8')
+
+        database = Database(db)
+        photos_list = database.execute_query("select image_uri from profile_media;")
+        user_photos =[]
+        for photo in photos_list: 
+            user_photos.append(photo[0])
+
+        logging.info("{} photo(s) found".format(len(photos_list)))
+        return user_photos
+
+
+
+
+    def get_bio_changes(self):
         
         logging.info("Get Biography Changes...")
-        user_profile ={}
         db = os.path.join(self.internal_cache_path, "databases", "tinder-3.db")
-        
         database = Database(db)
-        photos_list = database.execute_query("select image_uri from profile_media")
-        user_profile["photos_url"] = []
-        for photo in photos_list: user_profile["photos_url"].append(photo[0])
-
-        user_profile["biography_changes"] =[]
+        
+        biography_changes = []
         bio_list = database.execute_query("select old_bio, bio, timestamp from profile_change_bio order by timestamp")
         for entry in bio_list: 
             bio_change ={}
             bio_change["old"] = entry[0]
             bio_change["new"] = entry[1]
             bio_change["createdtime"] = entry[3]
-            user_profile["biography_changes"].append(bio_change)
-
-        return user_profile
+            biography_changes.append(bio_change)
+        
+        logging.info("{} biography change(s) found".format(len(biography_changes)))
+        return biography_changes
 
     def get_user_matches(self):
-        logging.info("Getting User Profiles...")
+        logging.info("Getting User Matches...")
         matches = {}
 
         db = os.path.join(self.internal_cache_path, "databases", "tinder-3.db")
@@ -111,11 +128,10 @@ class ModuleReport(ModuleParent):
         
 
         database = Database(db)
-        cards_list =[] #each entry means 1 conversation, including participants information and messages
-            #messages from conversations
+        cards_list =[] 
         cards = database.execute_query("select name_on_card, expiration_month, expiration_year, card_number_encrypted, date_modified, origin, use_count, use_date from credit_cards;")
         
-        #getting messages from conversations
+    
         for entry in cards:
             card={}
             card["name"] = entry[0]
@@ -127,7 +143,7 @@ class ModuleReport(ModuleParent):
             card["use_date"] = str(entry[7])
             cards_list.append(card)
 
-        logging.info("{} messages found".format(len(cards_list)))
+        logging.info("{} credit cards found".format(len(cards_list)))
 
         return cards_list
 
@@ -138,11 +154,10 @@ class ModuleReport(ModuleParent):
         db = os.path.join(self.internal_cache_path, "databases", "legacy_tinder-1.db")
 
         database = Database(db)
-        locations_list =[] #each entry means 1 conversation, including participants information and messages
-            #messages from conversations
+        locations_list =[]
         cards = database.execute_query("select latitude, longitude, state_province_long, country_short_name, country_long_name, address,route,street_number,city, last_seen_date/1000 as last_seen_date from tinder_locations;")
         
-        #getting messages from conversations
+        
         for entry in cards:
             location={}
             location["latitude"] = str(entry[0])
@@ -160,6 +175,10 @@ class ModuleReport(ModuleParent):
         logging.info("{} locations found".format(len(locations_list)))
 
         return locations_list
+
+    def get_sqlparse(self):
+        logging.info("Getting sqlparse...")
+        return Database.get_drp_output(self.databases, self.report_path)
         
 
 
